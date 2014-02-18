0d4f3920a6fa7677adb22384232dfcf61202b61b
Tom K. <tpk@kraussfamily.org>
2014-02-18 16:19:07 -0600

    - update email (motorola.com email no longer alive) / URL (point to google   code) in setup.py, bump version to 0.2.2 - update to more recent numpy.i file, to remove this warning:      Using deprecated NumPy API, disable it by #defining NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 #! /usr/bin/env python
 
 # Copyright (c) 2009, Motorola, Inc
-# 
+#
 # All Rights Reserved.
-# 
-# Redistribution and use in source and binary forms, with or without 
+#
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
-# 
-# * Redistributions in binary form must reproduce the above copyright 
-# notice, this list of conditions and the following disclaimer in the 
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
 # documentation and/or other materials provided with the distribution.
-# 
-# * Neither the name of Motorola nor the names of its contributors may be 
-# used to endorse or promote products derived from this software without 
+#
+# * Neither the name of Motorola nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
 # specific prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
-# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
@@ -41,13 +41,13 @@ within the distribution root.
 Usage:
   Commands are run from the distribution root directory.
 
-  python setup.py build_ext 
+  python setup.py build_ext
     builds the _Resampler extension and places it in the package directory
-    
-  python setup.py install 
-    builds the extension, installs the 'upfirdn' package in your python 
+
+  python setup.py install
+    builds the extension, installs the 'upfirdn' package in your python
     site-packages area
-    
+
 """
 # System imports
 import os, shutil
@@ -105,13 +105,13 @@ class custom_build_ext(build_ext):
         log.info('    from - %s'%ext_filename)
         log.info('      to - %s'%inplace_ext_filename)
         shutil.copy(ext_filename, inplace_ext_filename)
-        
+
 setup(name = "upfirdn",
-      version = "0.2.1",
+      version = "0.2.2",
       description = "An efficient polyphase FIR resampler",
       author      = "Tom Krauss and Bryan Nollett",
-      author_email = 'krauss@motorola.com',
-      url = 'http://opensource.motorola.com/sf/projects/upfirdn',
+      author_email = 'thomas.p.krauss@gmail.com',
+      url = 'http://code.google.com/p/upfirdn',
       license = 'Motorola BSD 2.0 [see LICENSE.txt]',
       py_modules = ['upfirdn.Resampler'],
       ext_modules = [_Resampler],
